d23dcabb49b8855742c8ca6ddbf233037ccca728
Vitali Kuntsevich <kvadesstudio@gmail.com>
2024-03-26 12:28:09 +0300

    feat: add change page.ts

--- src/app/page.ts
+++ src/app/page.ts
@@ -1,20 +1,20 @@
-import { main } from '@components/tags';
-import { movieService } from '@services/movie.service';
+import { main } from "@components/tags";
+import { movieService } from "@services/movie.service";
 
-import { BaseComponent } from './components/base-component';
-import { Header } from './components/header/header';
-import { MovieListPage } from './pages/movie-list';
-
+import { BaseComponent } from "./components/base-component";
+import { Header } from "./components/header/header";
+import { MovieListPage } from "./pages/movie-list";
+//By passing Header and main as constructor parameters, we explicitly declare the dependencies and improve the readability and maintainability of our code
 class PageWrapperComponent extends BaseComponent {
-  constructor() {
+  constructor(headerComponent, mainComponent) {
     super(
       {
-        className: 'page-wrapper',
+        className: "page-wrapper",
       },
-      Header.bind(null)(),
-      main.call(null, { className: 'main' }, MovieListPage(movieService)),
+      headerComponent(),
+      mainComponent({ className: "main" }, MovieListPage(movieService)),
     );
   }
 }
 
-export const PageWrapper = () => new PageWrapperComponent();
+export const PageWrapper = () => new PageWrapperComponent(Header, main);
