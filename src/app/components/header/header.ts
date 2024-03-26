61c1f894c4977077f48bbbb2a92b57123342aead
Vitali Kuntsevich <kvadesstudio@gmail.com>
2024-03-26 12:38:48 +0300

    refactor: header.ts

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -3,37 +3,19 @@ import { BaseComponent } from '@components/base-component';
 import { a, div, h2, img } from '@components/tags';
 
 import styles from './header.module.scss';
-
+//The initial if (false) condition seems unnecessary. Since it always evaluates to false, the code inside that block will never execute.
 export const Header = () => {
-  // eslint-disable-next-line no-constant-condition
-  if (false)
-    return new BaseComponent(
-      { tag: 'header', className: styles.header },
-      a({ href: '/movie-app/', className: styles.link }, h2(styles.title, 'Movie app')),
-      div(
-        {
-          className: styles.logo,
-        },
-        a(
-          { href: 'https://rs.school/js/', target: '_blank' },
-          img({
-            src: rsSchoolLogo,
-            alt: 'rs-school-logo',
-          }),
-        ),
-      ),
-    );
-  return new BaseComponent({
-    tag: 'header',
-    innerHTML: `<header class="${styles.header}">
-                <a href="/movie-app/" class="${styles.link}">
-                  <h2 class="${styles.title}">Movie app</h2>
-                </a>
-                <div class="${styles.logo}">
-                  <a href="https://rs.school/js/" target="_blank">
-                    <img src="${rsSchoolLogo}" alt="rs-school-logo">
-                  </a>
-                </div>
-              </header>`,
-  });
-};
+    return new BaseComponent({
+        tag: 'header',
+        innerHTML: `<header class="${styles.header}">
+                    <a href="/movie-app/" class="${styles.link}">
+                      <h2 class="${styles.title}">Movie app</h2>
+                    </a>
+                    <div class="${styles.logo}">
+                      <a href="https://rs.school/js/" target="_blank">
+                        <img src="${rsSchoolLogo}" alt="rs-school-logo">
+                      </a>
+                    </div>
+                  </header>`,
+      });
+    };
