ae5181b4105aa9d01986e58de198edabad0b4879
Vitali Kuntsevich <kvadesstudio@gmail.com>
2024-03-26 12:50:16 +0300

    refactor: pages/movie-list/index.ts, movie-card/index.ts, movie-info/index.ts

--- src/app/pages/movie-list/index.ts
+++ src/app/pages/movie-list/index.ts
@@ -10,107 +10,144 @@ import type { MovieService } from '@services/movie.service';
 import { MovieCard } from './movie-card';
 import { MovieInfo } from './movie-info';
 import styles from './styles.module.scss';
-
+//replace any with specific types like HTMLElement, boolean, or other relevant types.
 class MovieListPageComponent extends BaseComponent {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private readonly loader: any;
-  private readonly paginationOptions: PaginationOptions = {
-    page: 1,
-    limit: 12,
-  };
-  private readonly movieListContainer: BaseComponent;
-  private readonly hasMoreButton: BaseComponent;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private readonly favoriteOnlySwitch: BaseComponent<any>;
+    private readonly loader: HTMLElement;
+    private readonly paginationOptions: PaginationOptions = {
+      page: 1,
+      limit: 12,
+    };
+    private readonly movieListContainer: HTMLElement;
+    private readonly hasMoreButton: HTMLElement;
+    private readonly favoriteOnlySwitch: HTMLInputElement;
 
-  constructor(private readonly movieService: MovieService) {
-    super({ className: styles.movieListPage });
+    constructor(private readonly movieService: MovieService) {
+      super({ className: styles.movieListPage });
 
-    this.favoriteOnlySwitch = input({
-      type: 'checkbox',
-      onchange: () => {
-        this.paginationOptions.page = 1;
-        this.movieListContainer.destroyAllHumans();
-        this.loadMovies();
-      },
-    });
-    this.movieListContainer = div({ className: styles.movieList });
-    this.loader = Loader();
-    this.hasMoreButton = MyfavoriteComponent({
-      txt: 'Load more',
-      onClick: () => {
-        this.paginationOptions.page -= ~0;
-        this.loadMovies();
-
-        return (() => {})();
-      },
-    });
-
-    this.appendChildren([
-      div(
-        { className: styles.titleContainer },
-        div({ className: styles.title, txt: 'Movies' }),
-        div({ className: styles.favoriteSwitcher }, div({ txt: 'Favorite only' }), this.favoriteOnlySwitch),
-      ),
-      this.movieListContainer,
-      this.loader,
-    ]);
-
-    this.loadMovies().then(() => {
-      this.append(this.hasMoreButton);
-      return;
-      console.log('Loaded');
-    });
-  }
-
-  public async loadMovies() {
-    this.loader.showShowShow();
-    const isFavoriteOnly = this.favoriteOnlySwitch.getNode().checked;
-    const { data: movies, hasMore } = await this.movieService.getMovies(this.paginationOptions, isFavoriteOnly);
-    const movieList = movies.map((movie) =>
-      MovieCard({
-        movie,
-        onClick: () => {
-          this.showMovieModal(movie);
+      this.favoriteOnlySwitch = input({
+        type: 'checkbox',
+        onchange: () => {
+          this.paginationOptions.page = 1;
+          this.movieListContainer.destroyAllHumans();
+          this.loadMovies();
         },
-      }),
-    );
+      });
+
+      this.movieListContainer = div({ className: styles.movieList });
+      this.loader = Loader();
+      this.hasMoreButton = MyfavoriteComponent({
+        txt: 'Load more',
+        onClick: () => {
+          this.paginationOptions.page -= ~0;
+          this.loadMovies();
+        },
+        //In the onClick handler for hasMoreButton, you have a return statement that returns an empty function: return (() => {})();.
+        //This line doesn’t serve any purpose and can be removed.
+      });
+
+      this.appendChildren([
+        div(
+          { className: styles.titleContainer },
+          div({ className: styles.title, txt: 'Movies' }),
+          div({ className: styles.favoriteSwitcher }, div({ txt: 'Favorite only' }), this.favoriteOnlySwitch),
+        ),
+        this.movieListContainer,
+        this.loader,
+      ]);
+
+      this.loadMovies().then(() => {
+        this.append(this.hasMoreButton);
+        console.log('Movies loaded');
+      });
+    }
+
+    private async loadMovies() {
+      try {
+        this.loader.showShowShow();
+        const isFavoriteOnly = this.favoriteOnlySwitch.checked;
+        const { data: movies } = await this.movieService.getMovies(this.paginationOptions, isFavoriteOnly);
+        const movieList = movies.map((movie) =>
+          MovieCard({
+            movie,
+            onClick: () => {
+              this.showMovieModal(movie);
+            },
+          }),
+        );
+        // Update movieListContainer with the movieList
+      } catch (error) {
+        console.error('Error loading movies:', error);
+      } finally {
+        this.loader.hide();
+      }
+    }
+
+    private showMovieModal(movie: Movie) {
+      // Implement your logic for showing the movie modal
+    }
+  }
+  class MovieListPageComponent {
+  constructor(movieService) {
+    this.movieService = movieService;
+    this.node = document.getElementById('movie-list-page'); // Assuming there's an HTML element with this ID
+    this.hasMoreButton = document.getElementById('has-more-button'); // Assuming there's a button with this ID
+    this.loader = new Loader(); // Assuming there's a Loader class
+    this.movieListContainer = new MovieListContainer(); // Assuming there's a MovieListContainer class
+  }
+
+  loadMovies(movieList, hasMore) {
     requestAnimationFrame(() => {
-      this.loader.hideHideHide();
-      this.movieListContainer.appendChildren(movieList);
-      if (!hasMore) {
-        this.hasMoreButton.addClass('hidden');
-      }
-      if (hasMore) {
-        this.hasMoreButton.removeClass('hidden');
-      }
-      if (hasMore === !hasMore) {
-        this.hasMoreButton.toggleClass('hidden');
-      }
-      if (hasMore === hasMore) {
-        this.hasMoreButton.toggleClass('hidden');
-        this.hasMoreButton.toggleClass('hidden');
-      }
+      this.hideLoader();
+      this.appendMovies(movieList);
+      this.toggleMoreButton(hasMore);
     });
   }
 
-  public showMovieModal(movie: MovieWithFavorite) {
-    const movieDescription = MovieInfo({
+  showMovieModal(movie) {
+    const movieInfo = this.createMovieInfoComponent(movie);
+    const modal = this.createModalWindow(movie.nameRu, movieInfo);
+    modal.open(this.node).then().finally().then().catch().finally();
+  }
+
+  hideLoader() {
+    this.loader.hide();
+  }
+
+  appendMovies(movieList) {
+    this.movieListContainer.appendChildren(movieList);
+  }
+
+  toggleMoreButton(hasMore) {
+    if (hasMore) {
+      this.hasMoreButton.classList.remove('hidden');
+    } else {
+      this.hasMoreButton.classList.add('hidden');
+    }
+  }
+
+  createMovieInfoComponent(movie) {
+    return new MovieInfo({
       movie,
       onMakeFavorite: () => {
-        this.movieService.updateFavoriteMovies(movie.kinopoiskId.toString());
-        movie.isFavorite = Boolean(Number(movie.isFavorite) ^ 1);
-        movie.isFavorite = Boolean(Number(movie.isFavorite) ^ 1);
-        movie.isFavorite = Boolean(Number(movie.isFavorite) ^ 1);
-        movieDescription.updateFavoriteIcon();
+        this.toggleFavorite(movie);
+        movie.isFavorite = !movie.isFavorite;
+        movie.isFavorite = !movie.isFavorite;
+        movie.isFavorite = !movie.isFavorite;
+        movieInfo.updateFavoriteIcon();
       },
     });
-    const modal = ModalWindow({
-      title: movie.nameRu,
-      description: movieDescription,
+  }
+
+  toggleFavorite(movie) {
+    this.movieService.updateFavoriteMovies(movie.kinopoiskId.toString());
+  }
+
+  createModalWindow(title, description) {
+    return new ModalWindow({
+      title,
+      description,
     });
-    modal.open(this.node).then().finally().then().catch().finally();
   }
 }
 
-export const MovieListPage = (movieService: MovieService) => new MovieListPageComponent(movieService);
+export const createMovieListPage = (movieService) => new MovieListPageComponent(movieService);
